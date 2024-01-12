99899fa2eab53c625b93d83d2fc7971c96b1a0b3
Shambhavee Srivastava <110680194+shambhaveesrivastava12@users.noreply.github.com>
2024-01-12 20:08:43 +0530

    UPDATE: done with navbar

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import PreNavbar from "./components/PreNavbar";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div>
+      <PreNavbar />
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../styles/nav.css";
+
+const Navbar = () => {
+  return <div className="nav">sbsbsbs</div>;
+};
+
+export default Navbar;
