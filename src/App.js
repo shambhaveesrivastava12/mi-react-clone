71b02b425e41b02e67aa3252a8781e8be3d2ec32
Shambhavee Srivastava <110680194+shambhaveesrivastava12@users.noreply.github.com>
2024-01-13 12:15:01 +0530

    UPDATE: done with slider(carousel) ✅

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import "./App.css";
 import PreNavbar from "./components/PreNavbar";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router } from "react-router-dom";
+import Slider from "./components/Slider";
+// import banner from "./data/data.json";
+import data from "./data/data.json";
 
 function App() {
   return (
     <Router>
       <PreNavbar />
       <Navbar />
+      <Slider start={data.banner.start} />
     </Router>
   );
 }
