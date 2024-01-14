6687d6c7ba6d14c29787b5b419e7f17a50310417
Shambhavee Srivastava <110680194+shambhaveesrivastava12@users.noreply.github.com>
2024-01-14 10:17:20 +0530

    UPDATE: heading pattern established ✅

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Slider from "./components/Slider";
 // import banner from "./data/data.json";
 import data from "./data/data.json";
 import Offers from "./components/Offers";
+import Heading from "./components/Heading";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Navbar />
       <Slider start={data.banner.start} />
       <Offers offer={data.offer} />
+      <Heading text="STAR PRODUCTS" />
     </Router>
   );
 }
