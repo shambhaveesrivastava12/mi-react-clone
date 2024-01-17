391d8468f01fcf04a36c07b952a6afeeb8025c57
Shambhavee Srivastava <110680194+shambhaveesrivastava12@users.noreply.github.com>
2024-01-17 14:27:46 +0530

    UPDATE: done with IN THE PRESS section✅

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import HotAccessoriesMenu from "./components/HotAccessoriesMenu";
 import HotAccessories from "./components/HotAccessories";
 import ProductReviews from "./components/ProductReviews";
 import Videos from "./components/Videos";
+import Banner from "./components/Banner";
 
 function App() {
   return (
@@ -81,6 +82,8 @@ function App() {
       <ProductReviews productReviews={data.productReviews} />
       <Heading text="VIDEOS" />
       <Videos videos={data.videos} />
+      <Heading text="IN THE PRESS" />
+      <Banner banner={data.banner} />
     </Router>
   );
 }
